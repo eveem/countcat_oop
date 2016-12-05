1233dfe41889d073d6890a184a92030e77ce539c
eveem <monpriya.tammavong@gmail.com>
2016-12-05 20:00:00 +0700

    Add plus and minus images

--- board.py
+++ board.py
@@ -10,15 +10,23 @@ class MainScreen(arcade.Window):
 		arcade.set_background_color(arcade.color.BABY_PINK)
 
 		self.cat_sprites = []
-		for i in range(1, randint(1, 9)):
+
+		self.total_cat = randint(3, 9)
+		
+		self.plus = arcade.Sprite('images/plus.png')
+		self.plus.set_position(100, 100)
+
+		for i in range(1, self.total_cat):
 			self.cat_sprites.append(arcade.Sprite('images/' + str(randint(1, 12)) + '.png'))
-			self.cat_sprites[i - 1].set_position(randint(10, 1000) / 10 * 10, randint(10, 600) / 10 * 10)
+			self.cat_sprites[i - 1].set_position(randint(150, 850), randint(250, 475))
 
 	def on_draw(self):
 		arcade.start_render()
-		
+
 		for sprite in self.cat_sprites:
 			sprite.draw()
+
+		self.plus.draw()
 
 if __name__ == '__main__':
 	window = MainScreen(SCREEN_WIDTH, SCREEN_HEIGHT)
