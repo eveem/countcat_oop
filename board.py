4678ceed984f1f32b7a3f2112ff01299384d4de9
eveem <monpriya.tammavong@gmail.com>
2016-12-07 18:27:33 +0700

    Add number pictures and can use increase button

--- board.py
+++ board.py
@@ -9,6 +9,8 @@ class MainScreen(arcade.Window):
 		super().__init__(width, height)
 		arcade.set_background_color(arcade.color.BABY_PINK)
 
+		self.score_left = randint(0, 9)
+		self.score_right = randint(0, 9)
 		self.cat_sprites = []
 
 		self.total_cat = randint(3, 9)
@@ -24,7 +26,7 @@ class MainScreen(arcade.Window):
 		self.minus_right.set_position(700, 75)
 
 		for i in range(1, self.total_cat):
-			self.cat_sprites.append(arcade.Sprite('images/' + str(randint(1, 12)) + '.png'))
+			self.cat_sprites.append(arcade.Sprite('images/CAT' + str(randint(1, 12)) + '.png'))
 			self.cat_sprites[i - 1].set_position(randint(150, 850), randint(250, 475))
 
 	def on_draw(self):
@@ -38,6 +40,16 @@ class MainScreen(arcade.Window):
 
 		self.plus_right.draw()
 		self.minus_right.draw()
+
+		self.number_left = arcade.Sprite('images/' + str(self.score_left) + '.png')
+		self.number_left.set_position(200, 75)
+		self.number_left.draw()
+
+	def on_key_press(self, key, key_modifiers):
+		if key == arcade.key.A:
+			self.score_left += 1
+			if self.score_left > 9:
+				self.score_left = 0
 
 if __name__ == '__main__':
 	window = MainScreen(SCREEN_WIDTH, SCREEN_HEIGHT)
