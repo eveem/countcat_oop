915d72f7f8242f7140b5ab3fe9905d90518bace9
eveem <monpriya.tammavong@gmail.com>
2016-12-22 16:45:57 +0700

    Sprit class

--- board.py
+++ board.py
@@ -1,87 +1,89 @@
 import arcade
 from random import randint
 
+from models import World
+
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+	def __init__(self, *args, **kwargs):
+		self.model = kwargs.pop('model', None)
+
+		super().__init__(*args, **kwargs)
+
+	def sync_with_model(self):
+		if self.model:
+			self.set_position(self.model.x, self.model.y)
+
+	def draw(self):
+		self.sync_with_model()
+		super().draw()
 
 class MainScreen(arcade.Window):
 	def __init__(self, width, height):
 		super().__init__(width, height)
+		
 		arcade.set_background_color(arcade.color.BABY_PINK)
 
-		self.check_left = 0
-		self.check_right = 0
+		self.world = World(width, height)
 
-		start = randint(0, 9)
-		self.score_left = start
-		self.score_right = start
-		self.cat_sprites = []
+		# self.decrease_button = ModelSprite('images/minus.png', model=self.world.button)
+		# self.increase_button = ModelSprite('images/plus.png', model=self.world.button)
 
-		self.total_cat = randint(3, 9)
+		self.cats_sprite = []
+		for cat in self.world.cats:
+			self.cats_sprite.append(ModelSprite('images/CAT' + str(randint(1, 12)) + '.png', model=cat))
+
+		# self.check_left = 0
+		# self.check_right = 0
+
+		# start = randint(0, 9)
+		# self.score_left = start
+		# self.score_right = start
+		# self.cat_sprites = []
+
+		# self.total_cat = randint(3, 9)
 		
-		self.plus_left = arcade.Sprite('images/plus.png')
-		self.minus_left = arcade.Sprite('images/minus.png')
-		self.plus_left.set_position(300, 75)
-		self.minus_left.set_position(100, 75)
+		# self.plus_left = arcade.Sprite('images/plus.png')
+		# self.minus_left = arcade.Sprite('images/minus.png')
+		# self.plus_left.set_position(300, 75)
+		# self.minus_left.set_position(100, 75)
 
-		self.plus_right = arcade.Sprite('images/plus.png')
-		self.minus_right = arcade.Sprite('images/minus.png')
-		self.plus_right.set_position(900, 75)
-		self.minus_right.set_position(700, 75)
+		# self.plus_right = arcade.Sprite('images/plus.png')
+		# self.minus_right = arcade.Sprite('images/minus.png')
+		# self.plus_right.set_position(900, 75)
+		# self.minus_right.set_position(700, 75)
 
-		for i in range(1, self.total_cat):
-			self.cat_sprites.append(arcade.Sprite('images/CAT' + str(randint(1, 12)) + '.png'))
-			self.cat_sprites[i - 1].set_position(randint(150, 850), randint(250, 475))
+		# for i in range(1, self.total_cat):
+		# 	self.cat_sprites.append(arcade.Sprite('images/CAT' + str(randint(1, 12)) + '.png'))
+		# 	self.cat_sprites[i - 1].set_position(randint(150, 850), randint(250, 475))
 
 	def on_draw(self):
 		arcade.start_render()
 
-		for sprite in self.cat_sprites:
-			sprite.draw()
+		for cat in self.cats_sprite:
+			cat.draw()
 
-		self.plus_left.draw()
-		self.minus_left.draw()
+		# self.plus_left.draw()
+		# self.minus_left.draw()
 
-		self.plus_right.draw()
-		self.minus_right.draw()
+		# self.plus_right.draw()
+		# self.minus_right.draw()
 
-		self.number_left = arcade.Sprite('images/' + str(self.score_left) + '.png')
-		self.number_left.set_position(200, 75)
-		self.number_left.draw()
+		# self.number_left = arcade.Sprite('images/' + str(self.score_left) + '.png')
+		# self.number_left.set_position(200, 75)
+		# self.number_left.draw()
 
-		self.number_right = arcade.Sprite('images/' + str(self.score_right) + '.png')
-		self.number_right.set_position(800, 75)
-		self.number_right.draw()
+		# self.number_right = arcade.Sprite('images/' + str(self.score_right) + '.png')
+		# self.number_right.set_position(800, 75)
+		# self.number_right.draw()
 
-		arcade.draw_text(str(self.check_left), self.width - 60, self.height - 60, arcade.color.BLACK, 20)
+		# arcade.draw_text(str(self.check_left), self.width - 60, self.height - 60, arcade.color.BLACK, 20)
 		# arcade.draw_text(str(self.check_right), self.width - 60, self.height - 60, arcade.color.BLACK, 20)
 
 	def on_key_press(self, key, key_modifiers):
-		if key == arcade.key.A:
-			self.score_left -= 1
-		elif key == arcade.key.D:
-			self.score_left += 1
-
-		if key == arcade.key.J:
-			self.score_right -= 1
-		elif key == arcade.key.L:
-			self.score_right += 1
-
-		if self.score_left > 9:
-			self.score_left = 0
-		elif self.score_left < 0:
-			self.score_left = 9
-
-		if self.score_right > 9:
-			self.score_right = 0
-		elif self.score_right < 0:
-			self.score_right = 9
-
-		if key == arcade.key.S and self.score_left == self.total_cat - 1:
-			self.check_left += 1
-
-		if key == arcade.key.K and self.score_right == self.total_cat - 1:
-			self.check_right += 1
+		self.world.on_key_press(key, key_modifiers)
 
 if __name__ == '__main__':
 	window = MainScreen(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -7,8 +7,53 @@ class Model:
 		self.x = x
 		self.y = y
 
+class Cat(Model):
+	def __init__(self, world, x, y):
+		super().__init__(world, x, y)
+
+class World:
+	TOTAL_CAT = randint(0, 9)
+
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+
+		# self.increase_button = Button(self, 100, 75, 0)
+		# self.decrease_button = Button(self, 300, 75, 0)
+		# self.increase_button[1] = Button(self, 700, 75, 0)
+		# self.decrease_button[1] = Button(self, 900, 75, 0)
+
+		self.cats = []
+		for i in range(World.TOTAL_CAT):
+			cat = Cat(self, randint(150, 850), randint(250, 475))
+			self.cats.append(cat)
+
+		self.number = [0, 0]
+		self.point = [0, 0]
+
+	def on_key_press(self, key, key_modiriers):
+		if key == arcade.key.A:
+			self.number[0] -= 1
+		elif key == arcade.key.D:
+			self.number[0] += 1
+
+		if key == arcade.key.J:
+			self.number[1] -= 1
+		elif key == arcade.key.L:
+			self.number[1] += 1
+
+		for i in range(2):
+			if self.number[i] > 9:
+				self.number[i] = 0
+			if self.number[i] < 0:
+				self.number[i] = 9
+
+		if key == arcade.key.S and self.number[0] == self.total_cat - 1:
+			self.point[0] += 1
+
+		if key == arcade.key.K and self.number[1] == self.total_cat - 1:
+			self.point[1] += 1
+
 class Button(Model):
 	def __init__(self, world, x, y):
-		super().__init__(world, x, y, 0)
-
-	def 
+		super().__init__(world, x, y)
